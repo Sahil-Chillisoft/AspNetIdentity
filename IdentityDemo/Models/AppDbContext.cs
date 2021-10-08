d9ddbdac86ca576258e015b3b7ffd985dd5c97d1
Sahil Muniram <sahil.muniram@chillisoft.co.za>
2021-10-08 14:24:48 +0200

    AspNetIdentity demo project

---
+++ IdentityDemo/Models/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityDemo.Models
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<User>().Property(p => p.Id).HasColumnName("Usr_Id");
+            modelBuilder.Entity<User>().Property(p => p.FirstName).HasColumnName("Usr_FirstName").HasMaxLength(100);
+            modelBuilder.Entity<User>().Property(p => p.LastName).HasColumnName("Usr_LastName").HasMaxLength(100);
+            modelBuilder.Entity<User>().Property(p => p.PhoneNumber).HasColumnName("Usr_ContactNumber");
+            modelBuilder.Entity<User>().Property(p => p.Email).HasColumnName("Usr_Email");
+        }
+    }
+}
